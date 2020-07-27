3130a67c4e381f9bff494404f9324f8fab9bb879
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-27 20:47:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,48 @@
-# Add your code here
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+   
+   
+  def self.all
+    @@all
+  end
+  
+  
+  def self.clear_all
+   @@all.clear
+  end
+  
+  
+  def se
+    
+    
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Message jeffrey currie
+
+Details
+jeffrey currie
+
